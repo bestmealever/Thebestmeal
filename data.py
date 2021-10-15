3e6a7e4c1ba3c19859560d66206ffa8c1bfe4bbe
jieun-dev1 <jkdev1324@gmail.com>
2021-10-15 22:16:07 +0900

    로그인 기능 구현2

--- data.py
+++ data.py
@@ -5,12 +5,12 @@ import os
 data = pd.read_csv('food_final.csv', encoding='utf-8-sig', index_col=False)
 
 # 배포서버
-client = MongoClient('mongodb://test:test@15.164.212.139:27017')
+# client = MongoClient('mongodb://test:test@15.164.212.139:27017')
 
 # 로컬서버
-# client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017)
 
-db = client.bestmealever
+db = client.team_project
 
 for i in range(len(data)):
     doc = {'name': data.iloc[i]['title'], 'category': data.iloc[i]['category'].split(','),
