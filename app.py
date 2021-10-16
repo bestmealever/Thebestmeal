ab502f2a5ede38ce72dcbc749b1fd3d60fc83d14
장민 <>
2021-10-16 09:52:44 +0900

    수정 중

--- app.py
+++ app.py
@@ -1,164 +1,85 @@
 # -*- coding: utf-8 -*-
+import re
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+import boto3
 import random
 import os
+from datetime import datetime
 
 app = Flask(__name__)
+
 # 배포서버
-client = MongoClient(os.environ.get("MONGO_DB_PATH"))
+# client = MongoClient(os.environ.get("MONGO_DB_PATH"))
 
 # 로컬서버
-# client = MongoClient('localhost', 27017)
-
+client = MongoClient("mongodb://localhost:27017/")
 db = client.team_project
 
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html')
 
-class WhatYouWantForMeal:
-    def __init__(self):
-        self.for_reverse = ['korean', 'chinese', 'japanese', 'western', 'snack', 'bread', 'supper', 'fastfood', 'salad']
-        self.retry_num = 0
-        self.recommend = ''
-        self.address = ''
-        self.want = list()
-        self.feel = list()
-        self.chosen = list()
-        self.choice_num = list()
-
-    def want_receive(self, want_give):
-        self.want = want_give
-
-    def yesterday(self, yesterday_give):
-        yesterday_give_receive_reverse = []
-        for i in self.for_reverse:
-            if i not in yesterday_give:
-                yesterday_give_receive_reverse.append(i)
-            else:
-                pass
-        self.want = yesterday_give_receive_reverse
-
-    def yesterday_no(self):
-        self.want = ['korean', 'chinese', 'japanese', 'western', 'snack', 'bread', 'supper', 'fastfood', 'salad']
-
-    def feeling(self, feel):
-        self.chosen = []
-        self.feel = feel
-
-        for i in self.want:
-            for j in self.feel:
-                self.chosen.append(
-                    db.team_project.find_one({'category': i, 'emotion': j}, {'_id': False, 'name': True, 'url': True}))
-
-        if self.chosen == [None]:
-            self.chosen = [{'name': '추천 할게 없어요 ㅠㅠ',
-                            'url': 'https://blog.kakaocdn.net/dn/cCSIPC/btqKdFDO51a/vuyWbKS5CqBtWnDgyl3pv0/img.jpg'}]
-        else:
-            self.choice_num = [i for i in range(len(self.chosen))]
-            random.shuffle(self.choice_num)
-
-    def retry(self):
-        self.retry_num += 1
-
-
-what_you_want = WhatYouWantForMeal()
-
-
-# HTML을 주는 부분
-@app.route('/')
-def home():
-    global what_you_want
-    what_you_want = WhatYouWantForMeal()
-    print('객체 새로 만듬')
-    return render_template('index.html')
-
-
-@app.route('/kakao')
-def kakao():
-    return render_template('kakao.html')
-
-
-@app.route('/want', methods=['POST'])
-def want():
-    want_give_receive = request.form.getlist('want_give[]')
-    if want_give_receive == []:
-        return jsonify({'result': 'fail', 'msg': '하나 이상 선택해 주세요!'})
-    else:
-        what_you_want.want_receive(want_give_receive)
-        print(what_you_want.want)
+@app.route('/step1', methods=['POST'])
+def step1():
+    foodname_receive = request.form['foodname_give']
+    exists = bool(db.team_project.find_one({'name': foodname_receive}))
+    if exists == False :
+        global recoFoodName
+        recoFoodName = foodname_receive
+        print('없으니 등록')
         return jsonify({'result': 'success'})
-
-
-@app.route('/want_no', methods=['POST'])
-def want_no():
-    return {'msg': '먹고 싶은게 없다니...'}
-
-
-@app.route('/yesterday', methods=['POST'])
-def yesterday():
-    yesterday_give_receive = request.form.getlist('yesterday_give[]')
-    if yesterday_give_receive == []:
-        return jsonify({'result': 'fail', 'msg': '하나 이상 선택해 주세요!'})
     else:
-        what_you_want.yesterday(yesterday_give_receive)
-        print(what_you_want.want)
-        return jsonify({'result': 'success'})
+        print('있으니 거절')
+        return jsonify({'result': 'fail', 'msg': '이미 있는 음식이예요!'})
 
-
-@app.route('/yesterday_no', methods=['POST'])
-def yesterday_no():
-    what_you_want.yesterday_no()
-    print(what_you_want.want)
-    return {'msg': '어제 먹은게 기억이 안나요??'}
-
-
-@app.route('/feeling', methods=['POST'])
-def feeling():
-    feeling_give_receive = request.form.getlist('feeling_give[]')
-    if feeling_give_receive == []:
-        return jsonify({'result': 'fail', 'msg': '하나 이상 선택해 주세요!'})
+@app.route('/step2', methods=['POST'])
+def step2():
+    cat_give_receive = request.form.getlist('food_cat_give[]')
+    exists = bool(cat_give_receive)
+    if exists == False :
+        print('값을 선택해라')
+        return jsonify({'result': 'fail', 'msg': '1개 이상 선택해주세요!'})
     else:
-        what_you_want.feeling(feeling_give_receive)
-        if len(what_you_want.chosen) == 1:
-            return jsonify({'result': 'success', 'msg1': '', 'chosen': what_you_want.chosen[0], 'msg2': ''})
-        else:
-            num = what_you_want.retry_num
-            return jsonify(
-                {'result': 'success', 'msg1': '오늘은,', 'chosen': what_you_want.chosen[what_you_want.choice_num[num]],
-                 'msg2': '어때요?!'})
+        print('성공')
+        global recoFoodCate
+        recoFoodCate = cat_give_receive
+        return {"result": "success"}
 
-
-@app.route('/feeling_no', methods=['POST'])
-def feeling_no():
-    return {'msg': '하나 이상 선택해줘야 추천을 하지...!!!'}
-
-
-@app.route('/retry', methods=['POST'])
-def retry():
-    what_you_want.retry()
-    num = what_you_want.retry_num
-    if what_you_want.retry_num >= len(what_you_want.choice_num):
-        return jsonify({'result': 'success', 'msg1': '', 'chosen': {'name': '더 이상 추천 할게 없어요 ㅠㅠ',
-                                                                    'url': 'https://blog.kakaocdn.net/dn/cCSIPC/btqKdFDO51a/vuyWbKS5CqBtWnDgyl3pv0/img.jpg'},
-                        'msg2': ''})
+@app.route('/step3', methods=['POST'])
+def step3():
+    feel_give_receive = request.form.getlist('food_feel_give[]')
+    exists = bool(feel_give_receive)
+    if exists == False:
+        print('값을 선택해라')
+        return jsonify({'result': 'fail', 'msg': '1개 이상 선택해주세요!'})
     else:
-        return jsonify(
-            {'result': 'success', 'msg1': '그러면~', 'chosen': what_you_want.chosen[what_you_want.choice_num[num]],
-             'msg2': '어때요?!'})
+        global recoFoodFeel
+        recoFoodFeel = feel_give_receive
+        return {"result": "success"}
 
-
-@app.route('/to_kakao', methods=['POST'])
-def to_kakao():
-    what_you_want.address = request.form['address_give']
-    what_you_want.recommend = request.form['recommend_give']
-    return render_template('kakao.html')
-
-
-@app.route('/get_keyword', methods=['POST'])
-def get_keyword():
-    print('여기까진 ok')
-    search = f'{what_you_want.address} {what_you_want.recommend}'
-    print(search)
-    return {'search': search}
+@app.route('/fileupload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+    s3 = boto3.client('s3',
+                      aws_access_key_id="AKIAZBAKZL3E5QUOL3GH",
+                      aws_secret_access_key="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"
+                      )
+    s3.put_object(
+        ACL="public-read",
+        Bucket="jproject3",
+        Body=file,
+        Key=file.filename,
+        ContentType=file.content_type)
+    global recoFoodUrl
+    recoFoodUrl = 'https://jproject3.s3.ap-northeast-2.amazonaws.com/'+file.filename
+    doc = {'name': recoFoodName,
+           'category': recoFoodCate,
+           'emotion': recoFoodFeel,
+           'url': recoFoodUrl}
+    db.team_project.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 
 if __name__ == '__main__':
