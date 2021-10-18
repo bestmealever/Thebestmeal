6349f34268b21998d2bf8daca2b7e21e0e78f88b
장민 <>
2021-10-18 11:13:39 +0900

    포스팅 부분 기능 구현 2차 push

--- static/postingjs.js
+++ static/postingjs.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 function step1() {
     let foodname = $("#foodname1").val();
@@ -6,21 +5,6 @@ function step1() {
     $("#foodname").find("input[type=text]").each(function () {
         // 빈칸일 경우 경고
         if ($(this).val().trim() == '') {
-=======
-function step1() {
-    let btn_val = []
-    for (let i = 0; i < 9; i++) {
-        let btnCheck = $(`input:checkbox[id="chk${i + 1}"]`)
-        if (btnCheck.is(":checked") === true) {
-            btn_val.push(btnCheck.val())
-        }
-    }
-    console.log(btn_val)
-    let foodname = $("#foodname1").val();
-    $("#foodname").find("input[type=text]").each(function () {
-        if ($(this).val().trim() == '') {
-            // alert('음식 이름을 입력해주세요!');
->>>>>>> origin/jm-posting
             $("#modal-post").addClass("is-active");
         } else (
             $.ajax({
@@ -28,7 +12,6 @@ function step1() {
                 url: "/step1",
                 data: {foodname_give: foodname},
                 success: function (response) {
-<<<<<<< HEAD
                     // 이미 등록된 음식일 경우 경고
                     if (response['result'] === 'fail') {
                         $("#modal-post2").addClass("is-active");
@@ -64,49 +47,6 @@ function step1() {
                                           <div class="button-group-out">
                                              <button class="button next-stage" onclick="step2()">다음</button>
                                           </div>`
-=======
-                    if (response['result'] === 'fail') {
-                        // alert(response['msg'])
-                        $("#modal-post2").addClass("is-active");
-                        console.log(response['msg'])
-                    } else {
-                        let temp_html = `<div class="question-h">
-                    <p class="question-style"> Q.1 어떤 종류의 음식인가요? </p>
-                </div>
-                                            <div class="button-group-in">
-                                                <input type="checkbox" id="chk1" class="select-category" value="korean">
-                                                <label class="category" for="chk1">한식</label>
-                                
-                                                <input type="checkbox" id="chk2" class="select-category" value="chinese">
-                                                <label class="category" for="chk2">중식</label>
-                                
-                                                <input type="checkbox" id="chk3" class="select-category" value="japanese">
-                                                <label class="category" for="chk3">일식</label>
-                                            </div>
-                                            <div class="button-group-in">
-                                                <input type="checkbox" id="chk4" class="select-category" value="western">
-                                                <label class="category" for="chk4">양식</label>
-                                
-                                                <input type="checkbox" id="chk5" class="select-category" value="snack">
-                                                <label class="category" for="chk5">분식</label>
-                                
-                                                <input type="checkbox" id="chk6" class="select-category" value="bread">
-                                                <label class="category" for="chk6">빵</label>
-                                            </div>
-                                            <div class="button-group-in">
-                                                <input type="checkbox" id="chk7" class="select-category" value="supper">
-                                                <label class="category" for="chk7">야식</label>
-                                
-                                                <input type="checkbox" id="chk8" class="select-category" value="fastfood">
-                                                <label class="category" for="chk8">패스트푸드</label>
-                                
-                                                <input type="checkbox" id="chk9" class="select-category" value="salad">
-                                                <label class="category" for="chk9">샐러드</label>
-                                            </div>
-                                            <div class="button-group-out">
-                                             <button class="button next-stage" onclick="step2()">다음</button>
-                                            </div>`
->>>>>>> origin/jm-posting
                         let btnGroup = $('#button-group')
                         btnGroup.empty()
                         btnGroup.append(temp_html)
@@ -130,7 +70,6 @@ function step2() {
         url: "/step2",
         data: {food_cat_give: btn_val},
         success: function (response) {
-<<<<<<< HEAD
             // 선택하지 않았을 경우 경고
             if (response['result'] === 'fail') {
                 $("#modal-post3").addClass("is-active");
@@ -160,41 +99,6 @@ function step2() {
                 let btnGroup = $('#button-group')
                 btnGroup.empty()
                 btnGroup.append(temp_html)
-=======
-            if (response['result'] === 'fail') {
-                $("#modal-post3").addClass("is-active");
-            } else {
-                let temp_html = `<div class="question-h">
-                                    <p class="question-style"> Q.2 언제 먹으면 좋아요? </p>
-                                </div>
-                                <div class="button-group-in">
-                                    <input type="checkbox" id="chk1" class="select-category" value="no_time">
-                                    <label class="category" for="chk1">시간이 없어요</label>
-
-                                    <input type="checkbox" id="chk2" class="select-category" value="many_time">
-                                    <label class="category" for="chk2">시간 많아요!</label>
-
-                                    <input type="checkbox" id="chk3" class="select-category" value="perfect">
-                                    <label class="category" for="chk3">완벽한 저녁이 필요해요!</label>
-                                </div>
-                                <div class="button-group-in">
-                                    <input type="checkbox" id="chk4" class="select-category" value="needsugar">
-                                    <label class="category" for="chk4">당 떨어져요 ㅠ</label>
-
-                                    <input type="checkbox" id="chk5" class="select-category" value="stressed">
-                                    <label class="category" for="chk5">스트레스 받았어요</label>
-
-                                    <input type="checkbox" id="chk6" class="select-category" value="fatty">
-                                    <label class="category" for="chk6">기름진게 땡기네요!</label>
-                                </div>
-                                <div class="button-group-out">
-                                    <button class="button next-stage" onclick="step3()">다음</button>
-                                </div>`
-                let btnGroup = $('#button-group')
-                btnGroup.empty()
-                btnGroup.append(temp_html)
-
->>>>>>> origin/jm-posting
             }
         }
     })
@@ -215,7 +119,6 @@ function step3() {
         data: {food_feel_give: btn_val},
         success: function (response) {
             if (response['result'] === 'fail') {
-<<<<<<< HEAD
                 // 선택하지 않았을 경우 경고
                 $("#modal-post3").addClass("is-active");
             } else {
@@ -244,20 +147,6 @@ function step3() {
                                 <div class="button-group-out">
                                     <button type="button" class="button next-stage" onclick="save()">저장</button>
                                 </div>`
-=======
-                $("#modal-post2").addClass("is-active");
-            } else {
-                let temp_html = `
-        <div>
-            <div class="form-group">
-                <form id="upload-file">
-                    <label for="post-url">이미지 파일</label>
-                    <input type="file" name="file"/>
-                </form>
-            </div>
-            <button type="button" class="btn btn-primary" onclick="save()">저장</button>
-        </div>`
->>>>>>> origin/jm-posting
                 let btnGroup = $('#button-group')
                 btnGroup.empty()
                 btnGroup.append(temp_html)
@@ -265,7 +154,6 @@ function step3() {
         }
     })
 }
-<<<<<<< HEAD
 <!--이미지 미리보기 이벤트-->
 function setThumbnail(event) {
     var reader = new FileReader();
@@ -285,18 +173,12 @@ function save() {
     console.log(comment)
     form_data.append("comment_give", comment)
 
-=======
-
-function save() {
-    let form_data = new FormData($('#upload-file')[0]);
->>>>>>> origin/jm-posting
     $.ajax({
         type: 'POST',
         url: '/fileupload',
         data: form_data,
         processData: false,
         contentType: false,
-<<<<<<< HEAD
         success: function (response) {
             console.log(response['doc2'])
             let foodurl = response['doc2']['url']
@@ -325,10 +207,6 @@ function save() {
             let btnGroup = $('#button-group')
             btnGroup.empty()
             btnGroup.append(temp_html)
-=======
-        success: function (data) {
-            alert("파일이 업로드 되었습니다!!");
->>>>>>> origin/jm-posting
         },
     });
 }
