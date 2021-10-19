3c7b9cbf11efc9ee6b4b108b0e7d2d2601d4a824
hankug <hankug1@naver.com>
2021-10-19 09:25:21 +0900

    mypage db fail

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-from flask_cors import CORS
+# from flask_cors import CORS
+import boto3
 import random
 import os
-import boto3
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -17,13 +17,12 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'bestmealever'
 
-# DB 서버
+# 배포서버
 client = MongoClient('mongodb://test:test@15.164.212.139:27017')
 
 # 로컬서버
 # client = MongoClient('localhost', 27017)
 db = client.bestmealever
-
 
 class WhatYouWantForMeal:
     def __init__(self):
@@ -35,6 +34,9 @@ class WhatYouWantForMeal:
         self.feel = list()
         self.chosen = list()
         self.choice_num = list()
+        self.posting_food = ''
+        self.posting_category = ''
+        self.posting_emotion = ''
 
     def want_receive(self, want_give):
         self.want = want_give
@@ -73,15 +75,22 @@ class WhatYouWantForMeal:
 
 what_you_want = WhatYouWantForMeal()
 
-
-# HTML을 주는 부분
 @app.route('/')
 def home():
-    token_receive = request.cookies.get('mytoken')
     global what_you_want
-    what_you_want = WhatYouWantForMeal()
-    print('객체 새로 만듬')
-    return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+
+    if token_receive == None:
+        what_you_want = WhatYouWantForMeal()
+        return render_template('index.html')
+
+    else:
+        print(token_receive)
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user_info.find_one({"id": payload["id"]})
+        what_you_want = WhatYouWantForMeal()
+        return render_template('index-logged-in.html', user_info=user_info)
+
 
 @app.route('/recommend')
 def recommend():
@@ -91,10 +100,6 @@ def recommend():
 def kakao():
     return render_template('kakao.html')
 
-@app.route('/mypage')
-def mypage():
-    return render_template('mypage.html')
-
 #회원가입 및 로그인
 
 @app.route('/sign_in', methods=['POST'])
@@ -102,9 +107,13 @@ def sign_in():
     # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
+    print(username_receive)
+    print(password_receive)
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    print(pw_hash)
     result = db.user_info.find_one({'id': username_receive, 'pw': pw_hash})
+    print(result)
 
     if result is not None:
         payload = {
@@ -113,7 +122,7 @@ def sign_in():
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-        return jsonify({'result': 'success', 'token': token})
+        return jsonify({'result': 'success', 'token': token, 'id': result['id']})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
@@ -140,7 +149,6 @@ def sign_up():
         max_value = db.user_info.find_one(sort=[("idx", -1)])['idx'] + 1
 
     created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
 #idx 와 created at 해결 필요.
     doc = {
         "idx": max_value,
@@ -155,10 +163,6 @@ def sign_up():
     db.user_info.insert_one(doc)
 
     return jsonify({'result': 'success'})
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
 
 #추천 알고리즘
 
@@ -245,6 +249,83 @@ def get_keyword():
     print(search)
     return {'search': search}
 
+@app.route('/posting')
+def posting():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user_info = db.user_info.find_one({"id": payload["id"]})
+    return render_template('posting.html', user_info=user_info)
+
+@app.route('/step1', methods=['POST'])
+def step1():
+    foodname_receive = request.form['foodname_give']
+    exists = bool(db.food_info.find_one({'name': foodname_receive}))
+    if exists == False :
+        what_you_want.posting_food = foodname_receive
+        print('없으니 등록')
+        return jsonify({'result': 'success'})
+    else:
+        print('있으니 거절')
+        return jsonify({'result': 'fail', 'msg': '이미 있는 음식이예요!'})
+
+@app.route('/step2', methods=['POST'])
+def step2():
+    cat_give_receive = request.form.getlist('food_cat_give[]')
+    exists = bool(cat_give_receive)
+    if exists == False :
+        print('값을 선택해라')
+        return jsonify({'result': 'fail', 'msg': '1개 이상 선택해주세요!'})
+    else:
+        print('성공')
+        what_you_want.posting_category = cat_give_receive
+        return {"result": "success"}
+
+@app.route('/step3', methods=['POST'])
+def step3():
+    feel_give_receive = request.form.getlist('food_feel_give[]')
+    exists = bool(feel_give_receive)
+    if exists == False:
+        print('값을 선택해라')
+        return jsonify({'result': 'fail', 'msg': '1개 이상 선택해주세요!'})
+    else:
+        what_you_want.posting_emotion = feel_give_receive
+        return {"result": "success"}
+
+@app.route('/fileupload', methods=['POST'])
+def file_upload():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user_info = db.user_info.find_one({"id": payload["id"]})
+    file = request.files['file']
+    comment = request.form['comment_give']
+    s3 = boto3.client('s3',
+                      aws_access_key_id='...',
+                      aws_secret_access_key='...'
+                      )
+    s3.put_object(
+        ACL="public-read",
+        Bucket='...',
+        Body=file,
+        Key=file.filename,
+        ContentType=file.content_type)
+    posting_url = 'https://bestmealever-s3.s3.ap-northeast-2.amazonaws.com/'+file.filename
+    doc = {'name': what_you_want.posting_food,
+           'category': what_you_want.posting_category,
+           'emotion': what_you_want.posting_emotion,
+           'url': posting_url}
+    db.food_info.insert_one(doc)
+    print(doc)
+    doc2 = {'name': what_you_want.posting_food,
+           'category': what_you_want.posting_category,
+           'emotion': what_you_want.posting_emotion,
+           'url': posting_url,
+           'user_id' : user_info['id'],
+           'comment' : comment}
+    db.posting.insert_one(doc2)
+    print(doc2)
+    doc2 = db.posting.find_one({'name': what_you_want.posting_food}, {'_id': False})
+    return jsonify({'doc2':doc2})
+
 
 ##내 프로필 내에서만 수정버튼이 보일 수 있게 만든 api
 @app.route('/mypage/<username>')
@@ -271,26 +352,25 @@ def saving_update():
         if 'file_give' in request.files:
             file = request.files['file_give']
             s3 = boto3.client('s3',
-                              aws_access_key_id='...',
-                              aws_secret_access_key='...'
+                              aws_access_key_id='AKIAZSHMN4DACZOI3BHA',
+                              aws_secret_access_key='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'
                               )
             s3.put_object(
                 ACL="public-read",
-                Bucket="...",
+                Bucket="bestmealever-s3",
                 Body=file,
                 Key=file.filename,
                 ContentType=file.content_type
             )
             profile_url = 'https://bestmealever-s3.s3.ap-northeast-2.amazonaws.com/'+file.filename
-            new_doc = {'about': about_receive,
-                       'profile_pic': profile_url,
-                       'updated_at': datetime.now().strftime('%Y.%m.%d %H:%M:%S')
-                       }
-        db.user_info.update_one({'username': username}, {'$set':new_doc})
+            new_doc = {'profile': about_receive,
+                        'profile_pic': profile_url,
+                        'updated_at': datetime.now().strftime('%Y.%m.%d %H:%M:%S')
+                        }
+        db.user_info.update({'username': username}, new_doc)
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for('/'))
-
 ## 마이페이지에 내가 포스팅한 게시물만 보여주는 api
 # @app.route('/get_posts', methods=['GET'])
 # def get_posts():
